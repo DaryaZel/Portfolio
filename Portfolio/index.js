2b60b103e28d967edb6df916371ed12456437a2e
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-11-28 19:40:48 +0200

    fead: add resize display

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,3 +1,9 @@
-$('#myCollapsible').collapse({
-    toggle: false
-  })
+let displayButton=document.querySelector('#display-button')
+let backButton=document.querySelector('#back-button')
+let body=document.querySelector('body')
+
+displayButton.addEventListener('click',(e)=>{
+  document.body.classList.toggle('mobile-body');
+})
+backButton.addEventListener('click',(e)=>{
+})
