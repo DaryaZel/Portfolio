7bcfc4d078b2a789b56d1d3c522f56431ddc81e4
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-11-29 02:12:51 +0200

    feat: add displayButton style changes

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,9 +1,22 @@
 let displayButton=document.querySelector('#display-button')
 let backButton=document.querySelector('#back-button')
 let body=document.querySelector('body')
-
+let headerСontainer=document.querySelector('.header-container')
+let carouselControlPrev=document.querySelector('.carousel-control-prev')
+let carouselControlNext=document.querySelector('.carousel-control-next')
+let portfolioList=document.querySelectorAll('.portfolio-slider-container-list')
+let portfolioButton=document.querySelectorAll('.portfolio-slider-container-button')
+let portfolioSlider=document.querySelectorAll('.portfolio-slider-container')
+let portfolioImg=document.querySelectorAll('.portfolio-slider-container-img')
 displayButton.addEventListener('click',(e)=>{
-  document.body.classList.toggle('mobile-body');
+  body.classList.toggle('mobile-body');
+  headerСontainer.classList.toggle('mobile-header-container');
+  portfolioSlider.forEach(element => element.classList.toggle('mobile-carousel'));
+  carouselControlPrev.classList.toggle('mobile-carousel-items');
+  carouselControlNext.classList.toggle('mobile-carousel-items');
+  portfolioList.forEach(element => element.classList.toggle('mobile-carousel-items'));
+  portfolioButton.forEach(element => element.classList.toggle('mobile-carousel-items'));
+  portfolioImg.forEach(element => element.classList.toggle('mobile-portfolio-slider-container-img'));
 })
 backButton.addEventListener('click',(e)=>{
 })
